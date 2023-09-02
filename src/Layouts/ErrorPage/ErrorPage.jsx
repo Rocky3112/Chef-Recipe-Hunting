ce015650d1d9d25fee66d9e0dfe902c8914a0cd7
Rocky3112 <alomgirhossainrocky@gmail.com>
2023-09-02 23:55:30 +0600

    modified

--- src/Layouts/ErrorPage/ErrorPage.jsx
+++ src/Layouts/ErrorPage/ErrorPage.jsx
@@ -13,7 +13,7 @@ const ErrorPage = () => {
             <img className='' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCqLROK3HZMOi-8aauDabdfngqqHH20Cyquw&usqp=CAU" alt="" />
             </div>
             <div className='mt-10 flex items-center justify-center'>
-            <h1 className=' text-4xl text-purple-800 font-semibold'>404 Not Found</h1><br />
+            <h1 className=' text-4xl text-purple-700 font-semibold'>404 Not Found</h1><br />
             
         </div>
         <div className='flex items-center justify-center'>
